98224a1a09f35b620d40b2289bf32da070edd153
devgiljong <calf-love@naver.com>
2024-04-18 01:43:14 +0900

    :construction: Render success

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 "use client";
+
 import { useState } from "react";
 import { pdfjs, Document, Page } from "react-pdf";
+
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 import "react-pdf/dist/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
 
 export default function MyApp() {
-  const [numPages, setNumPages] = useState<number>();
-  const [pageNumber, setPageNumber] = useState<number>(1);
+  const [_, setNumPages] = useState<number>();
+  const [pageNumber] = useState<number>(1);
 
   function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
     setNumPages(numPages);
@@ -15,13 +17,14 @@ export default function MyApp() {
 
   return (
     <div>
-      <Document file="/resume.pdf" onLoadSuccess={onDocumentLoadSuccess}>
-        <Page pageNumber={pageNumber} />
-        <Page pageNumber={pageNumber + 1} />
+      <Document
+        className={" w-full flex justify-center items-center flex-col"}
+        file="/resume.pdf"
+        onLoadSuccess={onDocumentLoadSuccess}
+      >
+        <Page scale={2} pageNumber={pageNumber} />
+        <Page scale={2} pageNumber={pageNumber + 1} />
       </Document>
-      <p>
-        Page {pageNumber} of {numPages}
-      </p>
     </div>
   );
 }
