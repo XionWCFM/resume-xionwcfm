327add5c55e6cecd3aa92fc3ac0adb55f224e19a
devgiljong <calf-love@naver.com>
2024-04-20 15:08:20 +0900

    :construction: 커스텀훅으로 로직 분리

--- app/page.tsx
+++ app/page.tsx
@@ -1,64 +1,27 @@
-"use client";
+'use client';
 
-import { useEffect, useState } from "react";
-import { pdfjs, Document, Page } from "react-pdf";
+import { pdfjs, Document, Page } from 'react-pdf';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
-import "react-pdf/dist/Page/AnnotationLayer.css";
-import "react-pdf/dist/Page/TextLayer.css";
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+import 'react-pdf/dist/Page/TextLayer.css';
+import { useDocument, useWindowSize } from '~/src/shared/hooks';
 
-const getMediaQuery = (size: number) => {
-  if (size >= 1440) {
-    return 1024;
-  }
-
-  if (size >= 768) {
-    return 768;
-  }
-
-  return size;
-};
-
-export default function MyApp() {
-  const [_, setNumPages] = useState<number>();
-  const [pageNumber] = useState<number>(1);
-  const [windowSize, setWindowSize] = useState({ width: 0, height: 0 });
-  function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
-    setNumPages(numPages);
-  }
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      const handleResize = () => {
-        setWindowSize({
-          width: getMediaQuery(window.innerWidth),
-          height: getMediaQuery(window.innerHeight),
-        });
-      };
-
-      window.addEventListener("resize", handleResize);
-      handleResize();
-
-      return () => window.removeEventListener("resize", handleResize);
-    }
-  }, []);
+export default function MainPage() {
+  const { width, height } = useWindowSize();
+  const { onDocumentLoadSuccess, pageNumber } = useDocument();
+  const file = '/resume.pdf';
   return (
     <div>
       <main className="">
         <Document
-          className={" w-full flex justify-center items-center flex-col"}
-          file="/resume.pdf"
+          className={' w-full flex justify-center items-center flex-col'}
+          file={file}
           onLoadSuccess={onDocumentLoadSuccess}
         >
-          <Page
-            width={windowSize.width}
-            height={windowSize.height}
-            pageNumber={pageNumber}
-          />
-          <Page
-            width={windowSize.width}
-            height={windowSize.height}
-            pageNumber={pageNumber + 1}
-          />
+          {['page-1', 'page-2'].map((page, idx) => (
+            <Page key={page} width={width} height={height} pageNumber={pageNumber + idx} />
+          ))}
         </Document>
       </main>
     </div>
