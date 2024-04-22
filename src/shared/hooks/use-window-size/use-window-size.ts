b81a2ebb12423092bb6e3293f684a6b26691f86d
devgiljong <calf-love@naver.com>
2024-04-22 22:36:25 +0900

    :construction: 모바일 사용성 개선

--- src/shared/hooks/use-window-size/use-window-size.ts
+++ src/shared/hooks/use-window-size/use-window-size.ts
@@ -1,30 +1,62 @@
-import { useEffect, useState } from "react";
+import { useEffect, useLayoutEffect, useState } from 'react';
+
+const A4Ratio = 1.414;
+const MOBILE_WIDTH = 360;
+const TABLET_WIDTH = 768;
+const LABTOP_WIDTH = 1024;
+
+const A4SIZE = {
+  mobile: {
+    width: MOBILE_WIDTH,
+    height: MOBILE_WIDTH * A4Ratio,
+  },
+  tablet: {
+    width: TABLET_WIDTH,
+    height: TABLET_WIDTH * A4Ratio,
+  },
+  labtop: {
+    width: LABTOP_WIDTH,
+    height: LABTOP_WIDTH * A4Ratio,
+  },
+} as const;
+
+type WindowSizeType = {
+  width: number;
+  height: number;
+};
 
 export const useWindowSize = () => {
-  const [windowSize, setWindowSize] = useState({ width: 0, height: 0 });
-  const getMediaQuery = (size: number) => {
-    if (size >= 1440) {
-      return 1024;
+  const [windowSize, setWindowSize] = useState<WindowSizeType>(() => {
+    if (typeof window === 'undefined') {
+      return A4SIZE.mobile;
     }
-    if (size >= 768) {
-      return 768;
+    const currentWidthSize = window.innerWidth;
+    if (currentWidthSize >= LABTOP_WIDTH) {
+      return A4SIZE.labtop;
     }
-    return size;
-  };
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      const handleResize = () => {
-        setWindowSize({
-          width: getMediaQuery(window.innerWidth),
-          height: getMediaQuery(window.innerHeight),
-        });
-      };
+    if (currentWidthSize >= TABLET_WIDTH) {
+      return A4SIZE.tablet;
+    }
+    return A4SIZE.mobile;
+  });
 
-      window.addEventListener("resize", handleResize);
-      handleResize();
-
-      return () => window.removeEventListener("resize", handleResize);
+  useLayoutEffect(() => {
+    const handleResize = () => {
+      setWindowSize(() => {
+        const currentWidthSize = window.innerWidth;
+        if (currentWidthSize >= LABTOP_WIDTH) {
+          return A4SIZE.labtop;
+        }
+        if (currentWidthSize >= TABLET_WIDTH) {
+          return A4SIZE.tablet;
+        }
+        return A4SIZE.mobile;
+      });
+    };
+    if (typeof window !== undefined) {
+      window.addEventListener('resize', handleResize);
     }
-  }, []);
+    return () => window.removeEventListener('resize', handleResize);
+  }, [windowSize]);
   return windowSize;
 };
